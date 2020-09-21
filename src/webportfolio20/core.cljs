720a3638151517e95c1aeb2a355464ea33a6b500
Gustavo Araujo Borges <gustavoaraujob1+1@gmail.com>
2020-09-21 18:58:09 -0300

    Fix indenting

--- src/webportfolio20/core.cljs
+++ src/webportfolio20/core.cljs
@@ -8,56 +8,56 @@
 
 (defn profile-side-component []
   [:div.notification
-        [:div.card-image.has-text-centered
-         [:figure.image.is-128x128.is-inline-block
-          [:img.is-rounded {:src "https://www.jodilogik.com/wordpress/wp-content/uploads/2016/05/people-1.png"}]]]
-       [:p.title.has-text-centered "Guilherme de Paula"]
-       [:div.card-image.has-text-centered
-        [:div.columns.is-inline-flex
-         [:div.column
-          [:a.image.is-48x48 {:href "linkedin" :target "_blank"}
-           [:img {:src "http://www.prepare1.com/wp-content/uploads/2014/04/linkedin-logo-high-res-1254-1024x1024.jpg"}]]]
-         [:div.column
-          [:a.image.is-48x48 {:href "github" :target "_blank"}
-           [:img {:src "https://cdn.afterdawn.fi/v3/news/original/github-logo.png"}]]]
-         [:div.column
-          [:a.image.is-48x48 {:href "email" :target "_blank"}
-           [:img {:src "http://cdn.onlinewebfonts.com/svg/img_386071.png"}]]]]]
-       [:hr.has-background-grey-lighter]
-       [:p.subtitle.is-4 "Experience:"]
-       [:div
-        [:p.is-marginless.subtitle.is-5 [:strong "Client-Side:"]]
-        [:span [:p [:i.fas.fa-code] "\tClojureScript (Reagent)"]] [:br]
-        [:p.is-marginless.subtitle.is-5 [:strong "Server-side:"]]
-        [:span [:p [:i.fas.fa-server] "\tClojure"]] [:br]
-        [:p.is-marginless.subtitle.is-5 [:strong "Database:"]]
-        [:span [:p [:i.fas.fa-database] "\tMongoDB"]] [:br]
-        [:p.is-marginless.subtitle.is-5 [:strong "Other:"]]
-        [:span [:p [:i.fas.fa-plus-square] "\tPython"]]]])
+   [:div.card-image.has-text-centered
+    [:figure.image.is-128x128.is-inline-block
+     [:img.is-rounded {:src "https://www.jodilogik.com/wordpress/wp-content/uploads/2016/05/people-1.png"}]]]
+   [:p.title.has-text-centered "Guilherme de Paula"]
+   [:div.card-image.has-text-centered
+    [:div.columns.is-inline-flex
+     [:div.column
+      [:a.image.is-48x48 {:href "linkedin" :target "_blank"}
+       [:img {:src "http://www.prepare1.com/wp-content/uploads/2014/04/linkedin-logo-high-res-1254-1024x1024.jpg"}]]]
+     [:div.column
+      [:a.image.is-48x48 {:href "github" :target "_blank"}
+       [:img {:src "https://cdn.afterdawn.fi/v3/news/original/github-logo.png"}]]]
+     [:div.column
+      [:a.image.is-48x48 {:href "email" :target "_blank"}
+       [:img {:src "http://cdn.onlinewebfonts.com/svg/img_386071.png"}]]]]]
+   [:hr.has-background-grey-lighter]
+   [:p.subtitle.is-4 "Experience:"]
+   [:div
+    [:p.is-marginless.subtitle.is-5 [:strong "Client-Side:"]]
+    [:span [:p [:i.fas.fa-code] "\tClojureScript (Reagent)"]] [:br]
+    [:p.is-marginless.subtitle.is-5 [:strong "Server-side:"]]
+    [:span [:p [:i.fas.fa-server] "\tClojure"]] [:br]
+    [:p.is-marginless.subtitle.is-5 [:strong "Database:"]]
+    [:span [:p [:i.fas.fa-database] "\tMongoDB"]] [:br]
+    [:p.is-marginless.subtitle.is-5 [:strong "Other:"]]
+    [:span [:p [:i.fas.fa-plus-square] "\tPython"]]]])
 
 (defn project-component []
   [:div.notification
-        [:div.columns
-         [:div.column.is-one-third
-          [:figure.image {:style {:max-width "600px"}}
-           [:img {:src "https://cdn-images-1.medium.com/max/2600/1*S2mrAIUdexbLUjzNVVYdZg.png"}]]]
-         [:div.column
-          [:p.subtitle.is-4 [:strong "Webportfolio2.0"]]
-          [:p.has-text-justified "Project made in ClojureScript in conjuction with Reagent (ClojureScript React bindings)."][:br]
-          [:div.columns.is-flex
-           [:div.column
-            [:br]
-            [:p.has-text-right [:i "Data do Projeto: 2020"]] [:br]
-            [:div.has-text-right
-             [:div.field.is-grouped
-              [:div.control
-               [:button.button.is-primary [:strong  "Info"]]]
-              [:div.control
-               [:button.button.is-link.is-danger "Delete"]]]]]
-           [:div.column.is-one-fifth
-            [:figure.image.has-text-right
-             [:a.image.is-48x48 {:href "source" :target "_blank"}
-              [:img {:src "https://cdn.afterdawn.fi/v3/news/original/github-logo.png"}]]]]]]]])
+   [:div.columns
+    [:div.column.is-one-third
+     [:figure.image {:style {:max-width "600px"}}
+      [:img {:src "https://cdn-images-1.medium.com/max/2600/1*S2mrAIUdexbLUjzNVVYdZg.png"}]]]
+    [:div.column
+     [:p.subtitle.is-4 [:strong "Webportfolio2.0"]]
+     [:p.has-text-justified "Project made in ClojureScript in conjuction with Reagent (ClojureScript React bindings)."][:br]
+     [:div.columns.is-flex
+      [:div.column
+       [:br]
+       [:p.has-text-right [:i "Data do Projeto: 2020"]] [:br]
+       [:div.has-text-right
+        [:div.field.is-grouped
+         [:div.control
+          [:button.button.is-primary [:strong  "Info"]]]
+         [:div.control
+          [:button.button.is-link.is-danger "Delete"]]]]]
+      [:div.column.is-one-fifth
+       [:figure.image.has-text-right
+        [:a.image.is-48x48 {:href "source" :target "_blank"}
+         [:img {:src "https://cdn.afterdawn.fi/v3/news/original/github-logo.png"}]]]]]]]])
 
 (defn main-component []
   [:div.pagesize
@@ -65,9 +65,9 @@
     [:div.container
      [:div.columns
       [:div.column.is-one-third
-        [profile-side-component]]
+       [profile-side-component]]
       [:div.column
-        [project-component]]]]]])
+       [project-component]]]]]])
 
 (defn start []
   (reagent/render-component [main-component]
